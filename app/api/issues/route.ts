a199db4e4c973439a06f6d4b444d1ca5df650e1b
Zainab Khan <zainabfarooq431@gmail.com>
2025-02-19 22:34:01 +0500

    implement client side validation

--- app/api/issues/route.ts
+++ app/api/issues/route.ts
@@ -1,11 +1,7 @@
 import { NextRequest, NextResponse } from "next/server";
-import {z} from 'zod';
 import prisma from "@/prisma/client";
+import { createIssueSchema } from "../../ValidationSchema";
 
-const createIssueSchema= z.object({
-    title: z.string(). min(1, 'Title is required').max(255),
-description:z.string().min(1,'Description is required')
-})
 export async function POST(request: NextRequest){
     const body= await request.json();
    const validation=  createIssueSchema.safeParse(body);
--- app/issue/new/page.tsx
+++ app/issue/new/page.tsx
@@ -1,22 +1,24 @@
 'use client'
-import { TextField, Button, Callout } from '@radix-ui/themes';
+import { TextField, Button, Callout, Text } from '@radix-ui/themes';
 import SimpleMDE from 'react-simplemde-editor';
 import { useForm, Controller } from 'react-hook-form';
 import 'easymde/dist/easymde.min.css';
 import axios from 'axios';
 import { useRouter } from 'next/navigation';
 import { useState } from 'react';
+import { zodResolver } from '@hookform/resolvers/zod';
+import { createIssueSchema } from '@/app/ValidationSchema';
+import {z} from "zod";
 
-interface IssueForm {
-  title: string;
-  description: string;
-}
+type IssueForm=z.infer<typeof createIssueSchema>
 
 const NewIssuePage = () => {
     // eslint-disable-next-line @typescript-eslint/no-unused-vars
     const [error,setError] =useState('');
   const router = useRouter();
-  const { register, control, handleSubmit } = useForm<IssueForm>();
+  const { register, control, handleSubmit ,formState:{errors}} = useForm<IssueForm>({
+    resolver:zodResolver(createIssueSchema)
+  });
 
   const onSubmit = async (data: IssueForm) => {
     try {
@@ -41,14 +43,15 @@ const NewIssuePage = () => {
        <TextField.Root placeholder='Title' {...register('title')}>
         <TextField.Slot></TextField.Slot>
       </TextField.Root>
+      {errors.title && <Text color='red' as="p">{errors.title.message}</Text>}
 
       <Controller
         name="description"
         control={control}
         render={({ field }) => <SimpleMDE placeholder="Description" {...field} />}
       />
-
-      <Button type="submit">Submit</Button>
+{errors.description && <Text color='red' as="p">{errors.description.message} </Text>}
+      <Button type="submit">Submit Issue Now</Button>
     </form>
     </div>
   );
